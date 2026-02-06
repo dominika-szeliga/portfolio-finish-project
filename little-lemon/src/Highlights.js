9c371eb56a773a03c9234aee6b46effdd539f453
Dominika Szeliga <dominika.szeliga@onet.pl>
2026-02-06 10:12:58 +0100

    added more styles CSS

--- little-lemon/src/Highlights.js
+++ little-lemon/src/Highlights.js
@@ -2,27 +2,30 @@ function Highlights() {
     return(
 
         <>
-            <section className="main">
+            <section className="main_highlights">
                 <h1>This week's specials!</h1>
                 <button type="submit" className="button_main">Online menu</button>
               </section>
             <section className="highlights">
             <article>
+              <img src='salad.png' alt='salad'/>
             <h2>Greek salad</h2>
-            <img src='salad.png' alt='salad'/>
+            <h3>Extra price 12.99$</h3>
             <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutions.Order a delivery 
               <img src='delivery_icon.png' alt='icon'/>
             </p>
             </article>
           <article>
-            <h2>Bruschetta</h2>
             <img src='bruschetta.png' alt='bruschetta'/>
+            <h2>Bruschetta</h2>
+            <h3>Extra price 5.99$</h3>
             <p>Our Bruchetta is made from grilled bread that has been smeared with garlic
             and seasoned with salt and olive oil.Order a delivery <img src='delivery_icon.png' alt='icon'/></p>
           </article>
           <article>
-            <h2>Lemon Dessert</h2>
             <img src='lemon dessert.png' alt='lemon dessert'/>
+            <h2>Lemon Dessert</h2>
+            <h3>Extra price 5.00$</h3>
             <p>This comes straight from
           grandma’s recipe book every last ingredient has been sourced and is as authenics as can be imagined.
           Order a delivery <img src='delivery_icon.png' alt='icon'/></p>
